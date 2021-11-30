605f2f78b3dafbf8476ff5a3f9b8d17a18728deb
xwen3 <xwen3@mail.sfsu.edu>
2021-11-29 17:06:26 -0800

    visualizer simi done Kappa

--- client/src/visualizers/Rimanov.tsx
+++ client/src/visualizers/Rimanov.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:client/src/visualizers/Rimanov.tsx
 // 3rd party library imports
 import P5 from "p5";
 import * as Tone from "tone";
@@ -30,3 +31,40 @@ export const RimanovVisualizer = new Visualizer(
     p5.endShape();
   }
 );
+=======
+// 3rd party library imports
+import P5 from 'p5';
+import * as Tone from 'tone';
+
+// project imports
+import { Visualizer } from '../Visualizers';
+
+
+export const Xwen3Visualizer = new Visualizer(
+  'Xwen3Visualizer',
+  (p5: P5, analyzer: Tone.Analyser) => {
+    const width = window.innerWidth;
+    const height = window.innerHeight / 2;
+    p5.background(0, 0, 0, 225);
+    p5.noStroke();
+    p5.translate(width/2, height/2);
+    //let start = 0;
+    const space = 0.1;
+    const values = analyzer.getValue();
+    for (let i = 0; i < 360; i ++) {
+      const amplitude = values[i] as number;
+      const xoff = p5.map(p5.cos(i), -1, 1, 0, 3);
+      const yoff = p5.map(p5.sin(i), -1, 1, 0, 3);
+      const n = p5.noise(xoff, yoff);
+      const h = p5.map(n, 0, 1, -150, 150);
+      const r = p5.map(p5.sin(i), -1, 1, 100, 200);
+      const g = p5.map(h, -150, 150, 0, 150);
+      const b = p5.map(n, 0, 1, 150, 255);
+      p5.rotate(amplitude);
+      p5.fill(r, g, b);
+      p5.rect(200, 0, h, 1);
+    }
+    //start += 0.005;
+  }
+);
+>>>>>>> d8e93d0 (visualizer simi done Kappa):client/src/visualizers/Xwen3.tsx
