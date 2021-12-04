238997190bb430329eeb25653498d5b47a757c94
xwen3 <xwen3@mail.sfsu.edu>
2021-12-03 18:56:59 -0800

    added test songs in schema.sql and changed Nameless App to Hidden Leaf

--- client/src/SideNav.tsx
+++ client/src/SideNav.tsx
@@ -131,7 +131,7 @@ export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
   return (
     <div className="absolute top-0 left-0 bottom-0 w5 z-1 shadow-1 bg-white flex flex-column">
       <div className="h3 fw7 f5 flex items-center pl3 bb b--light-gray">
-        Nameless App
+        Hidden Leaf
       </div>
       <div className="flex-auto">
         <Instruments state={state} dispatch={dispatch} />
