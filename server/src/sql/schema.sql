4cbca84fd88eb146c1a0c945a8e910a3f35aaccc
Can Sirin <cansirin12@gmail.com>
2021-12-05 21:39:24 -0800

    remove we wish you a merry christmas song

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -22,7 +22,6 @@ VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4
        (5, 'Happy Birthday', 'C4 C4 D4 C4 F4 E4 C4 C4 D4 C4 G4 F4 C4 C4 C4 A4 G4 E4 D4 A5 A5 F4 G4 F4', 'Various Artist', 'No album'),
        (6, 'Twinkle Twinkle Little Star', 'C#4 C#4 G#4 G#4 A#4 A#4 G#4 F#4 F#4 F4 F4 D#4 D#4 C#4 G#4 G#4 F#4 F#4 F4 F4 D#4 G#4 G#4 F#4 F#4 F4 F4 D#4 C#4 C#4 G#4 G#4 A#4 A#4 G#4 F#4 F#4 F4 F4 D#4 D#4 C#4', 'Twinkle', 'No album'),
        (7, 'Spiderman', 'A4 C5 E5 D5 C5 A4 A4 C5 E5 F5 E5 D5 C5 A4 D5 F5 A5 G5 F5 D5 A4 C5 E5 F5 E5 D5 C5 A4 A4 E4 G4 B4 E5', 'Bob', 'Spiderman Theme'),
-       (8, 'Just Give me A Reason','B5 B4 G3 G3 D3 D4 B5 B4 A4 A4 F#3 F#3 C2 A4 A4 G4 G4 G4 G4 G4 F#4 E4 F#4 D3 D4 D4 C5 B4 A4 G4 B5 B4 G3 G4 D3 B4 B4 A4 A4 F#3 F#3 C3 C4 A5 G4 G4 G4 G4 F#4 E4 F#4 D3 D4 D4 C5 B4 A4 G4 G4', 'Pink - ft Nate Ruess', 'Pink'),
-       (9, 'We Wish You A Merry Christmas', 'G4 C4 C4 D4 C4 B4 A4 A4 A4 D4 D4 E4 D4 C4 B4 G4 G4 E4 E4 F4 E4 D4 C4 A4 G4 G4 A4 D4 B4 C4', 'Unknown', 'Unknown')
+       (8, 'Just Give me A Reason','B5 B4 G3 G3 D3 D4 B5 B4 A4 A4 F#3 F#3 C2 A4 A4 G4 G4 G4 G4 G4 F#4 E4 F#4 D3 D4 D4 C5 B4 A4 G4 B5 B4 G3 G4 D3 B4 B4 A4 A4 F#3 F#3 C3 C4 A5 G4 G4 G4 G4 F#4 E4 F#4 D3 D4 D4 C5 B4 A4 G4 G4', 'Pink - ft Nate Ruess', 'Pink')
 
 
